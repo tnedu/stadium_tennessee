ef74dd2ca2d695f78fa3e1720522585afe419935
thanuja bachala <thanuja.bachala@tnedu.gov>
2025-10-24 10:50:58 -0500

    TEDS-1197 - Fix the error to critical and fix the error message in 3009 error table._10_24_2025

--- dbt/models/business_rules/student_edorg_association/3009_stu_ed_org_at_school_level.sql
+++ dbt/models/business_rules/student_edorg_association/3009_stu_ed_org_at_school_level.sql
@@ -26,15 +26,14 @@ stg_student_edorgs as (
         where cast(seoa.school_year as int) between brule.error_school_year_start and brule.error_school_year_end
     )
 )
-
-/* Student/EdOrg Associations that are on school level */
+/* Errors Student/EdOrg Associations that are on school level */
 select distinct se.k_student, se.k_lea, cast( null as int ) as k_school, se.school_year,
-            {{ get_district_from_school_id('se.ed_org_id') }}  as ed_org_id, se.student_unique_id,
+     {{ get_district_from_school_id('se.ed_org_id') }}  as ed_org_id, se.student_unique_id,
     s.state_student_id as legacy_state_student_id,
     brule.tdoe_error_code as error_code,
     concat('Student ', 
         se.student_unique_id, '(', coalesce(s.state_student_id, '[no value]'), ') ',
-        'requires Student/EdOrg Association on District level but exists at school level.') as error,
+        'has School level Student/EdOrg Association that needs to be removed.') as error,
     brule.tdoe_severity as severity
 from stg_student_edorgs se
 join {{ ref('edu_edfi_source', 'stg_ef3__students') }} s
