8931cc8ce8dc2915bbe37ae4976df5b8e75e7431
thanuja bachala <thanuja.bachala@tnedu.gov>
2025-12-18 13:21:01 -0600

    TEDS-1248 - Refine the code in student_days for isa_member flag for sped_ Expelled_kids_generate_ADM_12_18_2025

--- dbt/models/ADM/2025/student_days.sql
+++ dbt/models/ADM/2025/student_days.sql
@@ -123,18 +123,15 @@ select k_student, k_lea, k_school, k_school_calendar,
             and is_early_graduate = 1 then 1
         else 0
     end as is_early_grad_date,
-    case
-        when is_expelled = 1 and coalesce(is_sped,0) = 1 
-             and calendar_date >= entry_date
-             and (exit_withdraw_date is null
-                  or calendar_date < exit_withdraw_date) then 1 
-        when is_expelled = 1 then 0
-        when is_funding_ineligible = 1 then 0
-        when calendar_date >= exit_withdraw_date 
-            and is_early_graduate = 1 then 1
-        when calendar_date >= entry_date
-            and (exit_withdraw_date is null
-                or calendar_date < exit_withdraw_date) then 1
+     case
+        when is_early_graduate = 1 and calendar_date >= exit_withdraw_date then 1
+        when calendar_date >= entry_date and (exit_withdraw_date is null or calendar_date < exit_withdraw_date) then
+            case
+                when is_expelled = 1 and coalesce(is_sped,0) = 1 then 1
+                when is_expelled = 1 then 0
+                when is_funding_ineligible = 1 then 0
+                else 1
+            end
         else 0
     end as isa_member
 from q
