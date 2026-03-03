34933237868e7ca633658b12f6ef2036d486f8f5
Steven McKee <steven.mckee@tnedu.gov>
2026-03-03 15:07:55 -0600

    TEDS-1157 - Fixed a bug where a new join was producing more records than it should

--- dbt/models/ADM/Gaps/adm_gaps_enrollments.sql
+++ dbt/models/ADM/Gaps/adm_gaps_enrollments.sql
@@ -10,6 +10,14 @@ with school_day_events as (
     select *
     from {{ ref('xwalk_calendar_events') }}
     where is_school_day = true
+),
+school_days as (
+    select dates.school_year, dates.k_school_calendar, dates.calendar_date
+    from {{ ref('stg_ef3__calendar_dates') }} dates
+    join {{ ref('stg_ef3__calendar_dates__calendar_events') }} date_events
+        on date_events.k_calendar_date = dates.k_calendar_date
+        and date_events.k_school_calendar = dates.k_school_calendar
+        and date_events.calendar_event in (select calendar_event_descriptor from school_day_events)
 )
 select ssa.school_year, ssa.k_student, sch.k_lea, ssa.k_school, ssa.school_Id, ssa.k_school_calendar,
     ssa.is_primary_school, ssa.entry_date, ssa.exit_withdraw_date,
@@ -22,15 +30,11 @@ select ssa.school_year, ssa.k_student, sch.k_lea, ssa.k_school, ssa.school_Id, s
 from {{ ref('stg_ef3__student_school_associations') }} ssa
 join {{ ref('stg_ef3__schools') }} sch
     on sch.k_school = ssa.k_school
-left outer join {{ ref('stg_ef3__calendar_dates') }} dates
+left outer join school_days dates
     on dates.school_year = ssa.school_year
     and dates.k_school_calendar = ssa.k_school_calendar
     and dates.calendar_date >= ssa.entry_date
     and (ssa.exit_withdraw_date is null 
         or dates.calendar_date < ssa.exit_withdraw_date)
-left outer join {{ ref('stg_ef3__calendar_dates__calendar_events') }} date_events
-    on date_events.k_calendar_date = dates.k_calendar_date
-    and date_events.k_school_calendar = dates.k_school_calendar
-    and date_events.calendar_event in (select calendar_event_descriptor from school_day_events)
 group by ssa.k_student, ssa.k_school, sch.k_lea, ssa.school_id, ssa.k_school_calendar,
     ssa.school_year, ssa.is_primary_school, ssa.entry_date, ssa.exit_withdraw_date
