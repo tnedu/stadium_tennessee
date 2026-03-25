01cef925efafb0e49864746ec73e8daf6114edcc
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-03-25 17:09:17 -0500

    TEDS-1190_1241 - mark 3008_3009 as potential, exclude potential in stEdOrg, stu_ed_org_unioned_in stg_data_errors.

--- dbt/models/business_rules/student_edorg_association/student_education_organization_associations.sql
+++ dbt/models/business_rules/student_edorg_association/student_education_organization_associations.sql
@@ -6,28 +6,5 @@
 }}
 
 select *
-from {{ ref('3001_immigrant_date_entered_us') }}
-union
-select *
-from {{ ref('3002_elb_date_entered_us') }}
-union
-select *
-from {{ ref('3003_no_native_language') }}
-union
-select *
-from {{ ref('3004_no_race') }}
-union
-select *
-from {{ ref('3005_cohort_year') }}
-union
-select *
-from {{ ref('3006_student_characteristics_end_date') }}
-union
-select *
-from {{ ref('3007_student_characteristics_overlaps') }}
-union
-select *
-from {{ ref('3008_no_student_ed_org_association') }}
-union
-select *
-from {{ ref('3009_stu_ed_org_at_school_level') }}
+from {{ ref('student_education_organization_associations_unioned') }}
+where tdoe_severity_code != 9
---
+++ dbt/models/business_rules/student_edorg_association/student_education_organization_associations_unioned.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized="table",
+    schema="stg_data_errors"
+  )
+}}
+
+select *
+from {{ ref('3001_immigrant_date_entered_us') }}
+union
+select *
+from {{ ref('3002_elb_date_entered_us') }}
+union
+select *
+from {{ ref('3003_no_native_language') }}
+union
+select *
+from {{ ref('3004_no_race') }}
+union
+select *
+from {{ ref('3005_cohort_year') }}
+union
+select *
+from {{ ref('3006_student_characteristics_end_date') }}
+union
+select *
+from {{ ref('3007_student_characteristics_overlaps') }}
+union
+select *
+from {{ ref('3008_no_student_ed_org_association') }}
+union
+select *
+from {{ ref('3009_stu_ed_org_at_school_level') }}
