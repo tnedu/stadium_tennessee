fef8aae54f7d094a8fb2a868356d80eacba1b5f2
thanujab-tnedu <thanuja.bachala@tnedu.gov>
2026-06-22 11:08:20 -0500

    Teds 1344 teds 1345 -- DateEnteredUS from StuEdOrgAsso, SSD from SSA (#70)
    * TEDS-1344 TEDS-1345 - Populate extensions in stage studentschoolassociations and edOrgAssociations
    
    * TEDS-1344 TEDS-1345 - fct_ssd to include all ssae ssds and ssa ssds
    
    * TEDS-1345 - fix schema name for SSA
    
    * TEDS-1345 - fix fct ssd model formating, remove k_school_xyear from partition.
    
    * TEDS-1344 DateEnteredUS_StudentEducationOrgAssociation
    
    * TEDS-1344 New brules 3001 & 3002, business rules seed
    
    * business_rules_year_ranges to include rule_model column.
    
    * Stage changes to work on BR's
    
    * TEDS-1344 Updated BRs 3001 & 3002 & business_rules_year_ranges
    
    * TEDS-1345 - fix rule_model to this.identifier.
    
    * TEDS-1344 student_education_organization_associations_errors_unioned updated with new brules
    
    * TEDS-1345 - SSA valid enroll for 3302, 3303 -- 3300 same as before.
    
    * TEDS-1344 Updated BR 3001 & 3002 removed extra joins
    
    * TEDS-1345 - Fix brule joins.
    
    * delete package-lock and dbt log files. 3303 format fix.
    
    * delete log file
    
    * TEDS-1345 - Fix 3300 where Attendance event = SSD.
    
    * TEDS-1345 - 3302 -fssd is left join - so brule join to populate error code.
    
    * Fix error_code column name in 3300 2026 in school attendance events
    
    * 3303 _ do not generate errors if student standard day itself is not present. generate only when sent and value is null or <= 0
    
    ---------
    
    Co-authored-by: pchhabria <pooja.chhabria@tnedu.gov>

---
+++ dbt/models/business_rules/student_edorg_association/3002_seoa_elb_date_entered_us.sql
@@ -0,0 +1,48 @@
+{{
+  config(
+    materialized="table",
+    schema="stg_data_errors"
+  )
+}}
+
+{% set error_code = 3002 %}
+
+/* Students with below LEP Codes must have Date Entered US populated. */
+with brule as (
+    select tdoe_error_code, 
+        cast(error_school_year_start as int) as error_school_year_start, 
+        cast(ifnull(error_school_year_end, 9999) as int) as error_school_year_end,
+        tdoe_severity,
+        rule_model
+    from {{ ref('business_rules_year_ranges') }} br
+    where br.tdoe_error_code = {{ error_code }}
+    and rule_model = '{{ this.identifier }}'
+),
+stg_student_edorgs as (
+    select 
+    seoa.*,
+    brule.tdoe_error_code,
+    brule.tdoe_severity
+    from {{ ref('stg_ef3__student_education_organization_associations') }} seoa
+    join brule
+        on cast(seoa.school_year as int)
+           between brule.error_school_year_start and brule.error_school_year_end
+    where seoa.k_lea is not null
+      and seoa.lep_code in ('L','W','1','2','3','4','F','N')
+),
+errors as (
+    select se.k_student, se.k_lea, se.k_school, se.school_year, se.ed_org_id, se.student_unique_id,
+        s.state_student_id as legacy_state_student_id,
+        se.tdoe_error_code as error_code,
+        concat('ELB Student ', 
+            se.student_unique_id, ' (', coalesce(s.state_student_id, '[no value]'), ') ',
+            'with LEP codes [L, W, 1, 2, 3, 4, F, N] require Date Entered US on District level Student/EdOrg Associations.') as error,
+        {{ severity_to_severity_code_case_clause('se.tdoe_severity') }},
+        se.tdoe_severity
+    from stg_student_edorgs se
+    join {{ ref('edu_edfi_source', 'stg_ef3__students') }} s
+        on se.k_student = s.k_student
+    where se.dateEnteredUS is null
+)
+select *
+from errors
---
+++ dbt/models/business_rules/student_school_association/3300_ssa_ssd_dates_within_enrollment.sql
@@ -0,0 +1,67 @@
+{{
+  config(
+    materialized="table",
+    schema="stg_data_errors"
+  )
+}}
+
+{% set error_code = 3300 %}
+
+with brule as (
+    select tdoe_error_code, 
+        cast(error_school_year_start as int) as error_school_year_start, 
+        cast(ifnull(error_school_year_end, 9999) as int) as error_school_year_end,
+        tdoe_severity,
+        rule_model
+    from {{ ref('business_rules_year_ranges') }} br
+    where br.tdoe_error_code = {{ error_code }}
+    and rule_model = '{{this.identifier}}'
+),
+ssas as (
+    select 
+        ssa.k_student, ssa.k_school, ssa.k_school_calendar, cast(ssa.school_id as int) as school_id,
+        ssa.student_unique_id, cast(ssa.school_year as int) as school_year, ssa.entry_date, 
+        ssa.exit_withdraw_date, ssa.entry_grade_level, ssa.calendar_code, ssa.studentStandardDays,
+        brule.tdoe_error_code, brule.tdoe_severity
+    from {{ ref('stg_ef3__student_school_associations') }} ssa
+    join brule brule
+        on cast(ssa.school_year as int) between brule.error_school_year_start and brule.error_school_year_end
+),
+ssa_ssd as (
+    select 
+        ssas.*,
+        sd.col.effectiveDate::date as ssd_date_start
+    from ssas
+    lateral view outer explode(studentStandardDays) sd
+),
+errors as (
+    /* Student Standard Day events must be within enrollment period. */
+    select 
+        ssd.k_student, 
+        ssd.k_school, 
+        ssd.k_school_calendar,
+        ssd.school_id,
+        ssd.student_unique_id,
+        ssd.school_year,
+        ssd.entry_date,
+        ssd.entry_grade_level,
+        ssd.calendar_code,
+        s.state_student_id as legacy_state_student_id,
+        ssd.tdoe_error_code as error_code, 
+        concat('Student Standard Day for Student ', 
+            ssd.student_unique_id, ' (', coalesce(s.state_student_id, '[no value]'), ') ',
+            'does not fall within Enrollment Period. Enrollment Start Date: ',
+            ifnull(ssd.entry_date, '[null]'), ', Enrollment End Date: ', 
+            ifnull(ssd.exit_withdraw_date, '[null]'), ', Student Standard Day Effective Date: ', 
+            ssd.ssd_date_start, '.') as error,
+        {{ severity_to_severity_code_case_clause('ssd.tdoe_severity') }},
+        ssd.tdoe_severity
+    from ssa_ssd ssd
+    join {{ ref('stg_ef3__students') }} s
+        on s.k_student = ssd.k_student
+    where (
+        not(ssd.ssd_date_start between ssd.entry_date 
+            and ifnull(ssd.exit_withdraw_date, to_date('9999-12-31','yyyy-MM-dd')))
+        )
+)
+select * from errors
