759b7e5d3a4b12c7ec7eb2c9716a8326fd1f9782
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-07-14 22:48:58 -0500

    TEDS-1381 - cds_student_club_ids, add extra columns.

---
+++ dbt/models/custom_data_sources/cds_student_club_ids.sql
@@ -0,0 +1,13 @@
+{{
+  config(
+    materialized="table",
+    schema="cds"
+  )
+}}
+select k_student, k_student_xyear, k_school, assigning_org,
+       max(case when id_system = 'CTEChapterid' then id_code end) as CTEChapterid,
+       max(case when id_system = 'CTEMembershipid' then id_code end) as CTEMembershipid
+from `teds_dev`.`dev_thanujab_stage`.`stg_ef3__stu_ed_org__identification_codes`
+where k_school is not null
+and id_system in ('CTEChapterid','CTEMembershipid')
+group by k_student, k_student_xyear, k_school, assigning_org
