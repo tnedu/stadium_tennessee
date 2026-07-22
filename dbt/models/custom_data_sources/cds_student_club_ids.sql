08b0a05db83fe8718fcaaf7dfd9e06fdc2be11f0
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-07-22 15:24:55 -0500

    TEDS-1381 -- cds primary keys should match the fct table primary keys.

--- dbt/models/custom_data_sources/cds_student_club_ids.sql
+++ dbt/models/custom_data_sources/cds_student_club_ids.sql
@@ -1,20 +1,26 @@
-{{
-  config(
-    materialized="table",
-    schema="cds"
-  )
-}}
-with cohort_ids as (
-  select distinct cohort_id, k_school
-  from {{ ref('stg_ef3__cohorts') }}
+with cohorts as (
+    select k_cohort, k_school, cohort_id
+    from {{ ref('dim_cohort') }}
+    where k_school is not null and k_lea is null
+),
+school_edorg_ids_cleaned as (
+    select ids.k_student, cohort.k_cohort, cohort.cohort_id,
+        case when id_system = 'CTEChapterid' then id_code end as CTEChapterId,
+        case when id_system = 'CTEMembershipid' then id_code end as CTEMembershipId
+    from {{ ref('stg_ef3__stu_ed_org__identification_codes') }} ids
+    join cohorts cohort
+        on cohort.k_school = ids.k_school
+        and upper(ids.assigning_org) LIKE '%' || upper(cohort.cohort_id) || '%'
+    where ids.k_school is not null
+    group by ids.k_student, cohort.k_cohort, cohort.cohort_id
+),
+formatted as (
+    select fsca.k_student, fsca.k_cohort, fsca.cohort_begin_date as cohort_begindate, 
+        CTE_cols.CTEChapterId, CTE_cols.CTEMembershipId
+    from {{ ref('stg_ef3__student_cohort_associations') }} fsca
+    join school_edorg_ids_cleaned CTE_cols
+        on CTE_cols.k_student = fsca.k_student
+        and CTE_cols.k_cohort = fsca.k_cohort
 )
-select seoic.k_student, seoic.k_student_xyear, seoic.k_school, seoic.assigning_org, cohort_ids.cohort_id,
-       max(case when seoic.id_system = 'CTEChapterid' then seoic.id_code end) as CTEChapterid,
-       max(case when seoic.id_system = 'CTEMembershipid' then seoic.id_code end) as CTEMembershipid
-from {{ ref('stg_ef3__stu_ed_org__identification_codes') }} seoic
-join cohort_ids
-  on seoic.k_school = cohort_ids.k_school
-  and upper(seoic.assigning_org) LIKE '%' || upper(cohort_ids.cohort_id) || '%'
-where seoic.k_school is not null
-and seoic.id_system in ('CTEChapterid','CTEMembershipid')
-group by seoic.k_student, seoic.k_student_xyear, seoic.k_school, seoic.assigning_org, cohort_ids.cohort_id
+select *
+from formatted
