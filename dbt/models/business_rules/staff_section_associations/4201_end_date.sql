2ddbdabc62bbbb16460f2b07a477ef02d62c006d
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-09-01 15:32:59 -0500

    TEDS-1420 - Fix error to return no value on null section Details for 4200, 4201, 4202.

--- dbt/models/business_rules/staff_section_associations/4201_end_date.sql
+++ dbt/models/business_rules/staff_section_associations/4201_end_date.sql
@@ -14,34 +14,33 @@ with brule as (
         tdoe_severity
     from {{ ref('business_rules_year_ranges') }} br
     where br.tdoe_error_code = {{ error_code }}
+        and rule_model = '{{ this.identifier }}'
 ),
 stg_staff_section_associations as (
-    select * from {{ ref('stg_ef3__staff_section_associations') }} ssa
+    select ssa.*,
+            brule.tdoe_error_code,
+            brule.tdoe_severity
+    from {{ ref('stg_ef3__staff_section_associations') }} ssa
+    join brule
+        on cast(ssa.school_year as int)
+           between brule.error_school_year_start and brule.error_school_year_end
     where ssa.end_date is not null
-        and exists (
-            select 1
-            from brule
-            where cast(ssa.school_year as int) between brule.error_school_year_start and brule.error_school_year_end
-    )
 ), errors as (
     /* Staff Section Begin Date must be within the school year begin and end date. */
     select ssa.k_staff, ssa.k_course_section, ssa.local_course_code, ssa.school_year, ssa.school_id, 
     ssa.section_id, ssa.session_name, ssa.staff_unique_id, ssa.begin_date,
-    brule.tdoe_error_code as error_code,
+    ssa.tdoe_error_code as error_code,
     concat('Staff Section Association End Date does not fall within the school year. Value Received: ', ssa.end_date, 
         '. The state school year starts ',
         concat((ssa.school_year-1), '-07-01'), ' and ends ', concat(ssa.school_year, '-06-30'),
-        ', Local Course Code: ', ssa.local_course_code, ', Section ID: ', ssa.section_id, '.') as error
+        ', Local Course Code: ', coalesce(ssa.local_course_code, '[no value]'),
+        ', Section ID: ', coalesce(ssa.section_id, '[no value]'), '.') as error,
+        {{ severity_to_severity_code_case_clause('ssa.tdoe_severity') }},
+        ssa.tdoe_severity
     from stg_staff_section_associations ssa
-    join brule
-        on ssa.school_year between brule.error_school_year_start and brule.error_school_year_end
     where 
         not(ssa.end_date between to_date(concat((ssa.school_year-1), '-07-01'), 'yyyy-MM-dd') 
             and to_date(concat(ssa.school_year, '-06-30'), 'yyyy-MM-dd'))
 )
-select errors.*,
-    {{ severity_to_severity_code_case_clause('brule.tdoe_severity') }},
-    brule.tdoe_severity
-from errors errors
-join brule
-    on errors.school_year between brule.error_school_year_start and brule.error_school_year_end
+select *
+from errors
