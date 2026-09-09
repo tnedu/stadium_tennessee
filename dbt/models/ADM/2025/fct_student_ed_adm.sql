fbff20d6a4f006c06fb9481713857b15fee2adde
thanuja bachala <thanuja.bachala@tnedu.gov>
2026-09-09 15:58:52 -0500

    TEDS-1430

--- dbt/models/ADM/2025/fct_student_ed_adm.sql
+++ dbt/models/ADM/2025/fct_student_ed_adm.sql
@@ -80,7 +80,7 @@ ed_ranges as (
             coalesce(end_date,'null'), ')'
         ) as sc_range
     from {{ ref('fct_student_characteristics') }}
-    where student_characteristic in ('I', 'J', 'H', 'U', 'FOS01', 'SN', 'TO')
+    where student_characteristic in ('I', 'J', 'H', 'U', 'FOS01', 'SN', 'TO','MF')
 ),
 contributing_eds as (
     select k_student, k_lea, school_year, report_period,
