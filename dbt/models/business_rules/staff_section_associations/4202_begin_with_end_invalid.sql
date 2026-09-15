3d3a6d7d6cbdcde58f6b150d4c6280f042b3e153
thanujab-tnedu <thanuja.bachala@tnedu.gov>
2026-09-15 11:10:34 -0500

    Teds 1420/add section details (#86)
    * TEDS-1420 - Add section Details for 4200, 4201, 4202.
    
    * TEDS-1420 - Add section Details for 4202.
    
    * TEDS-1420 - Add section Details for 4201.
    
    * TEDS-1420 - Fix error to return no value on null section Details for 4200, 4201, 4202.

--- dbt/models/business_rules/staff_section_associations/4202_begin_with_end_invalid.sql
+++ dbt/models/business_rules/staff_section_associations/4202_begin_with_end_invalid.sql
@@ -14,31 +14,31 @@ with brule as (
         tdoe_severity
     from {{ ref('business_rules_year_ranges') }} br
     where br.tdoe_error_code = {{ error_code }}
+        and rule_model = '{{ this.identifier }}'
 ),
 stg_staff_section_associations as (
-    select * from {{ ref('stg_ef3__staff_section_associations') }} ssa
+    select ssa.*,
+            brule.tdoe_error_code,
+            brule.tdoe_severity 
+    from {{ ref('stg_ef3__staff_section_associations') }} ssa
+    join brule
+        on cast(ssa.school_year as int)
+           between brule.error_school_year_start and brule.error_school_year_end
     where ssa.end_date is not null
-        and exists (
-            select 1
-            from brule
-            where cast(ssa.school_year as int) between brule.error_school_year_start and brule.error_school_year_end
-    )
 ), 
 errors as (
     /* End Date must be greater than Begin Date. */
     select ssa.k_staff, ssa.k_course_section, ssa.local_course_code, ssa.school_year, ssa.school_id, 
         ssa.section_id, ssa.session_name, ssa.staff_unique_id, ssa.begin_date,
-        brule.tdoe_error_code as error_code,
+        ssa.tdoe_error_code as error_code,
         concat('Staff Section Association End Date must be greater than the Begin Date. End Date received: ',
-            ssa.end_date, ', Begin Date: ', ssa.begin_date, '.') as error
+            ssa.end_date, ', Begin Date: ', ssa.begin_date, 
+            ', Local Course Code: ', coalesce(ssa.local_course_code, '[no value]'),
+            ', Section ID: ', coalesce(ssa.section_id, '[no value]'), '.') as error,
+        {{ severity_to_severity_code_case_clause('ssa.tdoe_severity') }},
+        ssa.tdoe_severity
     from stg_staff_section_associations ssa
-    join brule
-        on ssa.school_year between brule.error_school_year_start and brule.error_school_year_end
     where ssa.end_date < ssa.begin_date
 )
-select errors.*,
-    {{ severity_to_severity_code_case_clause('brule.tdoe_severity') }},
-    brule.tdoe_severity
-from errors errors
-join brule
-    on errors.school_year between brule.error_school_year_start and brule.error_school_year_end
+select *
+from errors
